907854f8983ba779a7ebe43432dfa9f94ce7b63f
Jair Júnior <jair@level131.com>
2019-10-28 14:40:31 -0300

    Updates

---
+++ precache-manifest.5c389e17f5990a10bc9a9e11a3e47ef9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c376f338d597f13985",
+    "url": "/css/app.f123d843.css"
+  },
+  {
+    "revision": "cbe6b2f4b58fadfd9954",
+    "url": "/css/chunk-vendors.ac1a332b.css"
+  },
+  {
+    "revision": "ec24a5d4c03b960e222f6b4d8486ab6f",
+    "url": "/img/piu-logo.svg"
+  },
+  {
+    "revision": "aa5909dbef7d36ec10783d0ce0bb990f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d98271a1a409b0f55c30",
+    "url": "/js/about.00cc430a.js"
+  },
+  {
+    "revision": "33c376f338d597f13985",
+    "url": "/js/app.92b157e5.js"
+  },
+  {
+    "revision": "cbe6b2f4b58fadfd9954",
+    "url": "/js/chunk-vendors.a1a4ddbc.js"
+  },
+  {
+    "revision": "59c6da6785dfb92311a13a1c4ec128aa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
